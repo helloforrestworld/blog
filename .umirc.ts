62d3124f1915b7eade1b95af51205b6fbe90113b
刘富炜 <fuwei.liu@happy-seed.com>
2021-02-26 15:46:00 +0800

    chore: add deploy workflow

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Blog',
   mode: 'site',
+  base: '/blog',
+  publicPath: '/blog/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // more config: https://d.umijs.org/config
 });
